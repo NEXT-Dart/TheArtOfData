b70615fa497bbedac84c0ca9d6b28184bd255c56
JJ-White <juriaanmoonen@hotmail.com>
2017-02-07 13:54:32 +0100

    Progress

--- TheArtOfData/TheArtOfDecoding/ImageParser.cs
+++ TheArtOfData/TheArtOfDecoding/ImageParser.cs
@@ -39,8 +39,8 @@ namespace TheArtOfDecoding
             imageFactory.EntropyCrop();
             
             imageFactory.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) , "crop.bmp"));
-            image = Image.FromFile(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "crop.bmp"));
-            Display();
+            //image = Image.FromFile(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "crop.bmp"));
+            //Display();
 
         }
 
@@ -53,7 +53,7 @@ namespace TheArtOfDecoding
             imageFactory.Brightness(15);
             imageFactory.Contrast(75);
 
-            //ResizeLayer resizeLayer = new ResizeLayer(new Size(pixelsPerRow * 5, 0));
+            //ResizeLayer resizeLayer = new ResizeLayer(new Size(pixelsPerRow, 0));
             //resizeLayer.ResizeMode = ResizeMode.Max;
             //imageFactory.Resize(resizeLayer);
 
@@ -64,7 +64,50 @@ namespace TheArtOfDecoding
 
         private void Read()
         {
+            Bitmap bmp = new Bitmap(image);
+            List<Color> pixels = new List<Color>();
+            int step = bmp.Width / pixelsPerRow;
+            int steps = 0;
+            int x = step / 2;
+            int y = x;
 
+            bool stop = false;
+            while (!stop)
+            {
+                pixels.Add(bmp.GetPixel(x, y));
+                x += step;
+                if (steps >= 9)
+                {
+                    y += step;
+                    x = step / 2;
+                    steps = 0;
+                }
+                if (y > bmp.Height)
+                    stop = true;
+                steps++;
+            }
+
+            ImageFromList(pixels);
+        }
+
+        private void ImageFromList(List<Color> list)
+        {
+            Bitmap img = new Bitmap(pixelsPerRow, ((int)((double)list.Count / pixelsPerRow) + 1));
+
+            int pixel = 0;
+            for(int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    img.SetPixel(x, y, list.ElementAt(pixel));
+                    if (pixel == list.Count - 1) break;
+                    else pixel++;
+                }
+                if (pixel > list.Count) break;
+            }
+
+            image = img;
+            image.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "i.bmp"));
         }
 
         private void Display()
