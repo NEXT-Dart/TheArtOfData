7824e986d36a4eb5e8c4514f0017d18c589183c2
Corpelijn <bassiecorpelijn@hotmail.com>
2017-02-07 11:19:59 +0100

    Added new Project

---
+++ TheArtOfData/TheArtOfDecoding/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheArtOfDecoding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if(open.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            ImageParser ip = new ImageParser(open.FileName);
+            Image img = ip.Run();
+        }
+    }
+}
---
+++ TheArtOfData/TheArtOfDecoding/ImageParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheArtOfDecoding
+{
+    class ImageParser
+    {
+        public ImageParser(string filename)
+        {
+
+        }
+
+        public Image Run()
+        {
+            return null;
+        }
+    }
+}
