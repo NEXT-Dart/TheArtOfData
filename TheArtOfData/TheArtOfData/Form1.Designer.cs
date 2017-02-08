65cb6a283ca9683737d594cb2906a2d602bba837
Corpelijn <bassiecorpelijn@hotmail.com>
2017-02-08 15:39:56 +0100

    `small bug fix

--- TheArtOfData/TheArtOfData/Form1.Designer.cs
+++ TheArtOfData/TheArtOfData/Form1.Designer.cs
@@ -183,7 +183,7 @@
             this.numericUpDown1.Size = new System.Drawing.Size(79, 20);
             this.numericUpDown1.TabIndex = 5;
             this.numericUpDown1.Value = new decimal(new int[] {
-            3,
+            10,
             0,
             0,
             0});
