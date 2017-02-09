19271173f2aadc1ff2ea870bf2aafe3dea1466e6
Corpelijn <bassiecorpelijn@hotmail.com>
2017-02-09 14:19:41 +0100

    bug fix

--- TheArtOfData/TheArtOfDecoding/InterlaceData.cs
+++ TheArtOfData/TheArtOfDecoding/InterlaceData.cs
@@ -60,45 +60,51 @@ namespace TheArtOfDecoding
         public Image DrawInterlace(Image source)
         {
             Bitmap bitmap = new Bitmap(source);
-            Bitmap cropped = new Bitmap(CropInfo.Width, CropInfo.Height);
-            Graphics g = Graphics.FromImage(cropped);
-
-            g.DrawImage(bitmap, 0, 0, CropInfo, GraphicsUnit.Pixel);
-
-            // Calculate the pixel width
-            const int scaleDeforming = 10;
-            PixelWidth = TotalImageWidth / PixelsPerRow;
-
-            int x = 0;
-            int y = 0;
-            lock (Colors)
+            try
             {
-                for (int i = 0; i < Colors.Count; i++)
+                Bitmap cropped = new Bitmap(CropInfo.Width, CropInfo.Height);
+                Graphics g = Graphics.FromImage(cropped);
+
+                g.DrawImage(bitmap, 0, 0, CropInfo, GraphicsUnit.Pixel);
+
+                // Calculate the pixel width
+                const int scaleDeforming = 10;
+                PixelWidth = TotalImageWidth / PixelsPerRow;
+
+                int x = 0;
+                int y = 0;
+                lock (Colors)
                 {
-                    if (i > Colors.Count)
-                        break;
-
-                    if (Colors[i] == -1)
-                        continue;
-
-                    Brush brush = new SolidBrush(Colors[i]);
-                    Pen pen = new Pen(Color.White, 3);
-                    //g.FillRectangle(brush, StartPosition.X + scaleDeforming / 2 + x * PixelWidth, StartPosition.Y + scaleDeforming / 2 + y * PixelWidth, PixelWidth - scaleDeforming, PixelWidth - scaleDeforming);
-                    //g.DrawRectangle(pen, StartPosition.X + scaleDeforming / 2 + x * PixelWidth, StartPosition.Y + scaleDeforming / 2 + y * PixelWidth, PixelWidth - scaleDeforming, PixelWidth - scaleDeforming);
-                    g.DrawRectangle(pen, scaleDeforming / 2 + x * PixelWidth, scaleDeforming / 2 + y * PixelWidth, PixelWidth - scaleDeforming, PixelWidth - scaleDeforming);
-
-                    x++;
-                    if (x == PixelsPerRow)
+                    for (int i = 0; i < Colors.Count; i++)
                     {
-                        x = 0;
-                        y++;
+                        if (i > Colors.Count)
+                            break;
+
+                        if (Colors[i] == -1)
+                            continue;
+
+                        Brush brush = new SolidBrush(Colors[i]);
+                        Pen pen = new Pen(Color.White, 3);
+                        //g.FillRectangle(brush, StartPosition.X + scaleDeforming / 2 + x * PixelWidth, StartPosition.Y + scaleDeforming / 2 + y * PixelWidth, PixelWidth - scaleDeforming, PixelWidth - scaleDeforming);
+                        //g.DrawRectangle(pen, StartPosition.X + scaleDeforming / 2 + x * PixelWidth, StartPosition.Y + scaleDeforming / 2 + y * PixelWidth, PixelWidth - scaleDeforming, PixelWidth - scaleDeforming);
+                        g.DrawRectangle(pen, scaleDeforming / 2 + x * PixelWidth, scaleDeforming / 2 + y * PixelWidth, PixelWidth - scaleDeforming, PixelWidth - scaleDeforming);
+
+                        x++;
+                        if (x == PixelsPerRow)
+                        {
+                            x = 0;
+                            y++;
+                        }
                     }
                 }
+
+
+                return cropped;
             }
-
-
-
-            return cropped;
+            catch (Exception)
+            {
+                return bitmap;
+            }
         }
 
         #endregion
